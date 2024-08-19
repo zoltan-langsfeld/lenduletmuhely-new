168116568345256c2dd065986aa4110f00b71e2a
Zoltan Lajtavari <zoltan.lajtavari@idealo.de>
2024-08-19 16:10:24 +0200

    Implements first version of the profile component

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -4,12 +4,18 @@ const Profile = ({ data }) => {
 	const { id, imgSrc, name, title, phone, email, detailsHref } = data;
 
 	return (
-		<div id={id}>
-			<img alt="image" src={imgSrc} />
-			<h2>{name}</h2>
-			<p>{title}</p>
+		<div id={id} className="flex flex-col justify-center items-center max-w-xs">
+			<img
+				className="w-36 h-36 rounded-full object-cover"
+				alt="profile_image"
+				src={imgSrc}
+			/>
+			<h2 className="text-3xl">{name}</h2>
+			<h3 className="text-xl">{title}</h3>
 			<p>
-				Telefonszám: <span>{phone}</span> <br />
+				Telefonszám: <span>{phone}</span>
+			</p>
+			<p>
 				E-mail: <span>{email}</span>
 			</p>
 			<p>
