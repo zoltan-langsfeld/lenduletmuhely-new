44c4db6e850ec67f5a12ed922b32748927cbb417
Zoltan Lajtavari <zoltan.lajtavari@idealo.de>
2024-08-20 15:10:40 +0200

    Adds adapter to do server side rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import react from "@astrojs/react";
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-	output: "server",
-	integrations: [react(), tailwind()],
+  output: "server",
+  integrations: [react(), tailwind()],
+  adapter: node({
+    mode: "standalone"
+  })
 });
