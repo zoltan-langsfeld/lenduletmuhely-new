3facf1093d3b5269bdbd1b5e2a9c45a885d4b911
Zoltan Lajtavari <zoltan.lajtavari@idealo.de>
2024-08-29 17:41:39 +0200

    Implements the first version of the navbar.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Profile = () => {
+	const [showServices, setShowServices] = useState(false);
+
+	const toggleServices = () => {
+		setShowServices(!showServices);
+		// biome-ignore lint/suspicious/noConsoleLog: <explanation>
+		console.log("clicked");
+	};
+
 	return (
-		<div>
-			<h1>Munkatársaink</h1>
-			<h1>Szolgáltatások</h1>
+		<div className="flex items-center gap-2">
+			<h1>Lendület Műhely</h1>
+			<h3>Munkatársaink</h3>
+			<button type={"button"} onClick={toggleServices}>
+				Szolgáltatások
+			</button>
+			{showServices && (
+				<div className="mt-4 p-4 border rounded bg-gray-100">
+					<p>Here are our services...</p>
+				</div>
+			)}
+			<h3>Áraink</h3>
+			<h3>Galéria</h3>
+			<h3>Rólunk</h3>
+			<h3>Kapcsolat</h3>
 		</div>
 	);
 };
